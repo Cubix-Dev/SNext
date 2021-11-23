d183c15d26f7c894b345d96c0ad3a2484ae8c943
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2021-11-23 21:53:19 +0000

    switched to TypeScript
    we are still compiling to JS tho

---
+++ src/ts/main.ts
@@ -0,0 +1,22 @@
+function grabVersionInfo(gameName) {
+    // TODO: can't complete until game file grabbing's done
+}
+
+// TODO: add system to grab game files from server
+
+async function convToJS(fileURL, fileName) {
+    console.log("Converting project....");
+
+    const { Project } = require("../sb-edit");
+    const fs = require("fs");
+    const path = require("path");
+
+    const file = fs.readFileSync(path.join(fileURL, fileName));
+    const project = await Project.fromSb3(file);
+
+    console.log(project);
+
+}
+
+convToJS("src\\exampleProject\\", "LemonAttack;1.0.sb3");
+
