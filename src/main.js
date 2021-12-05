b853d8342339e26f3aa575d7d5bda8b66bb39415
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2021-12-05 21:20:08 +0000

    broken file getter
    yh

--- src/main.js
+++ src/main.js
@@ -14,5 +14,33 @@ function grabVersionInfo(gameName) {
 }
 
 // TODO: add system to grab game files from server
+function getGameFiles(gameName) {
+    // i yoinked this from the front end team, just gonna be honest
+    var app = require('express')();
+    var multer = require('multer');
+    var upload = multer();
+    var f = require('fs');
+    let gamePath = "";
 
-grabVersionInfo("LemonAttack;27.sb3");
+    switch(gameName) {
+        case "MegaManScratchWars":
+            gamePath = "games/MegaManScratchWars;0.017.sb3";
+            break;
+        case "SonicEvo":
+            gamePath = "games/SonicEvo;1.1.2.sb3";
+            break;
+        case "SonicUnleashed":
+            gamePath = "games/SonicUnleased;0.3.0.sb3";
+            break;
+    }
+
+    const https = require('https')
+    const options = {
+    hostname: 'replit.com/@Simple21/ScratchNext-GameBase',
+    port: 3000,
+    path: gamePath,
+    method: 'GET'
+    };
+}
+grabVersionInfo("LemonAttack;27.sb3"); // when using this function, make sure to include the full file name!!!!
+getGameFiles("MegaManScratchWars"); // when using *this* function, type the full game name in PascalCase!
