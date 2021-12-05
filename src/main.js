a6df918890f16828db61cd53baf996983ba6dcf0
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2021-12-05 17:26:46 +0000

    remove more artifacts

--- src/main.js
+++ src/main.js
@@ -14,20 +14,6 @@ function grabVersionInfo(gameName) {
 
 // TODO: add system to grab game files from server
 
-/* async function convToJS(fileURL, fileName) {
-    console.log("Converting project....");
-
-    const { Project } = require("sb-edit");
-    const fs = require("fs");
-    const path = require("path");
-
-    const file = fs.readFileSync(path.join(fileURL, fileName));
-    const project = await Project.fromSb3(file);
-
-    console.log(project);
-
-} */
-
 grabVersionInfo("LemonAttack;27.sb3");
-// convToJS("src\\exampleProject\\", "LemonAttack;1.0.sb3");
+
 
