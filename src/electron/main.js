8046bc2e2db03d759fed5eb747c5c82e3be0c265
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2021-12-11 22:54:02 +0000

    ELECTRONJS WORKS

---
+++ src/electron/main.js
@@ -0,0 +1,29 @@
+/* all of this is just boilerplate to get electron working, no touchy please */
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600, 
+    frame: false // hides annoying debug stuff
+  })
+
+  win.loadFile('ui/index.html')
+}
+
+// when this promise is resolved (which should be kinda fast), the window will open.
+app.whenReady().then(() => {
+  createWindow()
+
+  // for macos; opens a window if there aren't any open on boot (how macos handles windows is weird dont blame me)
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// for windows and linux; ends the process if all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
