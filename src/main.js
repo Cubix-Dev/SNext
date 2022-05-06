08da96d0b7f088feaabd84a9c6e51c567226e8c1
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-05-06 19:01:35 -0400

    Fix menubar not working
    Kiwi would be proud.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
+const path = require("Path")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+var IMG_DIR = /icons/
 
 function createWindow () {
     // Create the browser window.
@@ -12,6 +15,7 @@ function createWindow () {
         height: 720,
         frame: false,
         backgroundColor: '#FFF',
+        icon: path.join(__dirname, IMG_DIR, snextlogo.png),
         webPreferences: {
             nodeIntegration: true
         }
@@ -25,37 +29,27 @@ function createWindow () {
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
 
+    // Create the window
+    app.on('ready', createWindow)
+
+
+
     // Emitted when the window is closed.
     mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
+        if(ProcessingInstruction.platform !== 'darwin'){
+            app.quit();
+        }
     });
 
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// Retrieve focused window
+var theWindow = BrowserWindow.getFocusedWindow();
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Execute common tasks
+// Minimize
+theWindow.minimize();
+// Maximize app
+theWindow.maximize();
+// Close app
+theWindow.close();
