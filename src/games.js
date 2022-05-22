0754e0996555cdb7042ad1dc91ecd8c78a7864b0
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-05-22 14:48:09 -0400

    Maybe load games into cache?
    hopefully.

--- src/games.js
+++ src/games.js
@@ -11,8 +11,32 @@ console.log(files);
 for (var i;i<files.length;i++) {
     console.log(files[i]);
     // element='<div id="game" style="width: 150px; height: 175px; display: inline-block; margin-left: 20px;" data-number="1"><img src="Image Link" style="width: 150px; height: 150px;" id="ID"><p id="'+files[i]+'"style="font-family: "Segoe UI Bold"; font-size: 15px; text-align: center; margin-top: -3px">'+files[i]+'</p></div>';
-    element = files[i];
-    document.getElementById('games').innerHTML += element;
+    fs.readFile(i, function(err, data) {
+        if (err) throw err;
+        JSZip.loadAsync(data).then(function (zip) {
+        files = Object.keys(zip.files);
+        // --> Move the files to the correct location so the console can load it
+        const dir = 'GameMemory/' + i
+        try {
+            // first check if directory already exists
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+                console.log("Directory is created.");
+            } else {
+                console.log("Directory already exists.");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        // --> Stack overflow ftw. Tbh idk if this will actually work. 
+        Object.keys(contents.files).forEach(function(filename) {
+            zip.file(filename).async('nodebuffer').then(function(content) {
+                fs.writeFileSync(dir, content);
+            });
+        });        
+        console.log(files);
+        });
+    });
 }
 
 //          \(*O*)/
