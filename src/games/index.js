4a47bb0abe9a8c62e82e04b06457ddcfcb5d5c53
Itz-Electro <jerrytodd2005@gmail.com>
2022-08-20 15:11:04 -0500

    Added back button to library.html + Color change

--- src/games/index.js
+++ src/games/index.js
@@ -1,1 +1,7 @@
 // Loop throught the GameMemory Folder and append the library.html accordingly
+const remote = require('@electron/remote');
+const win = remote.getCurrentWindow();
+
+function mainmenu() {
+    win.loadFile("./index.html")
+}
