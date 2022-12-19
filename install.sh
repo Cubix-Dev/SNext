4e42a57a31d0cc15dc2fcf87bb2111cd7d47a8df
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2022-12-19 20:45:01 +0000

    i should've tested this atleast once
    but i fixed it

--- install.sh
+++ install.sh
@@ -1,26 +1,31 @@
 # installing node
-if [[ command --version node &>/dev/null; ]] then
-        echo "Node isn't installed on this machine. Node is required for SNext to boot."
-        if [["$OSTYPE" == "linux-gnu"*]]; then
-            echo "We will attempt to install Node with apt-get. Should we? [Y/N]"
-            read confirm | tr '[:upper:]' '[:lower:]'
-            if [[ confirm == "Y" ]]; then
-                echo "Installing Node ..."
-                # this part will only work on debian-based distros but who cares
-                curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-                sudo apt-get install nodejs -y
-                echo "Node has been installed."
-                sleep 5
-            else
-                echo "Please install Node.js, then retry this installer."
-                exit 1
+if [ which node > /dev/null ] 
+then
+    echo "Node isn't installed on this machine. Node is required for SNext to boot."
+    if [ "$OSTYPE" == "linux-gnu"* ]
+    then
+        echo "We will attempt to install Node with apt-get. Should we? [Y/N]"
+        read confirm | tr '[:upper:]' '[:lower:]'
+        if [ confirm == "Y" ]
+        then
+            echo "Installing Node ..."
+            # this part will only work on debian-based distros but who cares
+            curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+            sudo apt-get install nodejs -y
+            echo "Node has been installed."
+            sleep 5
         else
             echo "Please install Node.js, then retry this installer."
             exit 1
+        fi
     else
+        echo "Please install Node.js, then retry this installer."
+        exit 1
+    fi
+else
         echo "Node has already been installed."
         sleep 5
-    fi
+fi
 
 
 echo "Installing required dependencies for SNext..."
@@ -31,7 +36,8 @@ echo "Fixing potential vulnerabilities..."
 npm audit fix --force
 
 # linux only
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then 
+if [ "$OSTYPE" == "linux-gnu"* ]
+then 
     # Fix chrome-sandbox issue
     echo "Preparing Sandbox..."
     cd ./node_modules
